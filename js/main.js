af66903ad9d0563a68d14a2e0de6d421cd50320b
Yash Shah <yashah1234@gmail.com>
2020-06-08 22:45:39 +0530

    added copy to clipboard

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
   console.log(`Why are you here? Don't try and do funny stuff`);
 });
+
+function copyToClipboard() {
+  navigator.permissions.query({name: "clipboard-write"}).then(result => {
+    if (result.state === "granted" || result.state === "prompt") {
+      navigator.clipboard.writeText(window.location.href)
+    }
+  });
+}
