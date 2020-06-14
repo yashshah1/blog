14380524ae2a291b3c300d93edda38eea27c6244
Ved Dandekar <veddandekar6@gmail.com>
2020-06-15 00:19:38 +0530

    feat: web-share api and toast notif
    use web-share api when available and display toast notif informing user that url has been copied to clipboard

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
-  console.log(`Why are you here? Don't try and do funny stuff`);
+	console.log(`Why are you here? Don't try and do funny stuff`);
 });
 
-function copyToClipboard() {
-  navigator.permissions.query({name: "clipboard-write"}).then(result => {
-    if (result.state === "granted" || result.state === "prompt") {
-      navigator.clipboard.writeText(window.location.href)
-    }
-  });
+function shareArticle() {
+	if (navigator.share)
+		navigator.share({
+			title: "Blog article",
+			url: window.location.href,
+		});
+	else
+		navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+			if (result.state === "granted" || result.state === "prompt") {
+				navigator.clipboard.writeText(window.location.href);
+				const toast = document.getElementById("toast");
+				toast.className = "show";
+				setTimeout(function () {
+					toast.className = toast.className.replace("show", "");
+				}, 3000);
+			}
+		});
 }
